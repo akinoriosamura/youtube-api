640f0114e63884f53aa6d4eb54851b64d6ca6d79
akinoriosamura <osamura.akinori@gmail.com>
2020-10-07 22:46:10 +0900

    update merge csv

--- merge_csv.py
+++ merge_csv.py
@@ -2,16 +2,16 @@ import os
 import time
 import pandas as pd
 
-target_dir = "../..//Downloads/cosme_youtuber"
+target_dir = "./洗顔料_comments"
 csv_files = os.listdir(target_dir)
 
 df_merged = None
-columns = ['id', 'channelId', 'title', 'description', 'viewCount', 'likeCount', 'dislikeCount','favoriteCount', 'commentCount', 'publishedAt']
+# columns = ['id', 'channelId', 'title', 'description', 'viewCount', 'likeCount', 'dislikeCount','favoriteCount', 'commentCount', 'publishedAt']
 for id_f, csv_f in enumerate(csv_files):
     if '.csv' in csv_f:
         print(csv_f)
         _df = pd.read_csv(os.path.join(target_dir, csv_f))
-        _df.columns = columns
+        # _df.columns = columns
         print(_df.shape)
         if id_f == 0:
             df_merged = _df
@@ -19,5 +19,5 @@ for id_f, csv_f in enumerate(csv_files):
             df_merged = pd.concat([df_merged, _df])
         print(df_merged.shape)
 import pdb;pdb.set_trace()
-df_merged.to_csv("merged_cosme_youtuber.csv", mode='a', index=None)
+df_merged.to_csv("merged_facewash_comments.csv", mode='a', index=None)
 
